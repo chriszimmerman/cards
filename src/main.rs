0b448d3e3940a3363a94d39f3aa36909bdc81a6e
Chris Zimmerman <chris@chriszimmerman.net>
2026-03-26 23:33:54 -0400

    Add scores up for each card drawn

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,12 @@ fn main() {
                 cards: Deck::generate_deck(),
             };
             deck.shuffle(&mut rng);
-            deck
+            let mut state = GameState {
+                deck: deck,
+                score: 0,
+                hand: Vec::new(),
+            };
+            state
         })
         .add_systems(Update, mouse_button_input)
         .add_systems(Startup, setup)
@@ -27,24 +32,24 @@ enum Suit {
     Spades,
 }
 
-#[derive(Debug, EnumIter, Clone, Display)]
+#[derive(Debug, EnumIter, Clone, Display, Ord, Eq, PartialOrd, PartialEq)]
 enum Rank {
-    Two,
-    Three,
-    Four,
-    Five,
-    Six,
-    Seven,
-    Eight,
-    Nine,
-    Ten,
-    Jack,
-    Queen,
-    King,
-    Ace,
+    King = 13,
+    Queen = 12,
+    Jack = 11,
+    Ten = 10,
+    Nine = 9,
+    Eight = 8,
+    Seven = 7,
+    Six = 6,
+    Five = 5,
+    Four = 4,
+    Three = 3,
+    Two = 2,
+    Ace = 1,
 }
 
-#[derive(Debug, Component)]
+#[derive(Debug, Component, Clone)]
 struct Card {
     suit: Suit,
     rank: Rank,
@@ -54,6 +59,13 @@ struct Card {
 #[derive(Debug, Component, Resource)]
 struct Deck {
     cards: Vec<Card>,
+}
+
+#[derive(Debug, Component, Resource)]
+struct GameState {
+    deck: Deck,
+    score: u32,
+    hand: Vec<Card>,
 }
 
 impl Deck {
@@ -120,18 +132,54 @@ fn mouse_button_input(
     buttons: Res<ButtonInput<MouseButton>>,
     mut commands: Commands,
     asset_server: Res<AssetServer>,
-    mut deck: ResMut<Deck>,
+    mut state: ResMut<GameState>,
 ) {
     if buttons.just_released(MouseButton::Left) {
-        let card = deck.cards.pop().unwrap();
+        let card = state.deck.cards.pop().unwrap();
         commands.spawn((
             Sprite {
                 image: asset_server.load(card.image.clone()),
                 custom_size: Some(Vec2::new(64., 96.)),
                 ..default()
             },
-            Transform::from_xyz(70. * (52. - deck.cards.len() as f32), 0., 0.),
+            Transform::from_xyz(70. * (52. - state.deck.cards.len() as f32), 0., 0.),
         ));
         println!("{} of {}", card.rank.to_string(), card.suit.to_string());
+        state.hand.push(card);
+        state.score = hand_score(state.hand.clone());
+        println!("Current score is {}", state.score);
+    }
+}
+
+fn hand_score(mut hand: Vec<Card>) -> u32 {
+    let mut score = 0;
+    hand.sort_by(|a, b| a.rank.cmp(&b.rank).reverse());
+    for card in hand.iter() {
+        let card_score = match card.rank {
+            Rank::Jack => 10,
+            Rank::Queen => 10,
+            Rank::King => 10,
+            Rank::Ten => 10,
+            Rank::Nine => 9,
+            Rank::Eight => 8,
+            Rank::Seven => 7,
+            Rank::Six => 6,
+            Rank::Five => 5,
+            Rank::Four => 4,
+            Rank::Three => 3,
+            Rank::Two => 2,
+            Rank::Ace => calculate_ace(score)
+        };
+        score += card_score;
+    }
+
+    score
+}
+
+fn calculate_ace(score: u32) -> u32 {
+    if score > 10 {
+        score + 1
+    } else {
+        score + 11
     }
 }
